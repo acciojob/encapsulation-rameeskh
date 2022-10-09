ce6b9b52267185627971ee01b55fbbf3c0ab8267
RAMEES K H <khramees.ramees@gmail.com>
2022-10-09 11:17:34 +0530

    encapsulation working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw=new RWOnly();
+
+//        System.out.println(rw.name);
+        //java: name has private access in com.driver.RWOnly
+
+        System.out.println(rw.getName());
+
+        rw.setName("Ricardo Kaka");
+        System.out.println(rw.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public RWOnly() {
+        name="Ramees";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
